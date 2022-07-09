14a5dae8120a43afd952e89004edf0a9305585ae
aheisleycook <aheisleycook@gamestation.attlocal.net>
2022-07-08 18:07:17 -0700

    gamelist style

--- gamelist/catalog/views.py
+++ gamelist/catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-def home(reques):
-    return render(template_name='home.html')
+def home(request):
+    return render(request,template_name='home.html')
 # Create your views here.
--- gamelist/gamelist/settings.py
+++ gamelist/gamelist/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'gamelist.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/catalog/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
